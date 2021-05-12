df3a6bb1934338462d8b654f852792359823cb1a
Larry Hodges <74116412+imhotep-hodges@users.noreply.github.com>
2021-05-12 00:06:11 -0400

    Add files via upload

--- portfolio/stocksdb/views.py
+++ portfolio/stocksdb/views.py
@@ -3,17 +3,6 @@ from django.shortcuts import redirect
 from .models import Portfolio
 from .forms import StockForm
 from django.contrib import messages
-from django.views.generic import TemplateView
-
-class StockChartView(TemplateView):
-   template_name='home.html'
-
-   def get_context_data(self,**kwargs):
-     context=super().get_context_data(**kwargs)
-     context["qs"] = Portfolio.objects.all()
-
-     return context
-   
 
 def home(request):
    import requests
